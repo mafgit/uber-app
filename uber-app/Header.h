e6f5a74038326e9d0de6b1b460df118823e6e636
mafgit <mafishere@gmail.com>
2023-03-02 18:30:28 +0500

    Meeting 1: edit and added a few classes added menu1 and menu2 functions

--- uber-app/Header.h
+++ uber-app/Header.h
@@ -3,17 +3,35 @@
 using namespace std;
 
 class Passenger {
-
+    int id, age;
+    string firstName, lastName, phoneNum, password;
 };
 
-class Driver {
 
+class Driver {
+    int id, age, sumOfRatings, userCountOfRating;
+    string firstName, lastName, phoneNum;
 };
 
 class Vehicle {
+    int model, noOfSeats;
+    string brand, plateNum, color;
+};
+
+class Car {
+
+};
+
+class Bike {
+
+};
+
+class Rickshaw {
 
 };
 
 class Booking {
+    string type;
+    char pickup, dropoff;
 
 };
--- uber-app/uber-app.cpp
+++ uber-app/uber-app.cpp
@@ -2,7 +2,46 @@
 #include "Header.h"
 using namespace std;
 
+int menu1()
+{
+	int opt;
+
+   do{
+	   cout << "Enter a number for one of the following commands:" << endl;
+		cout << "1) Signup" << endl;
+		cout << "2) Login" << endl;
+		cout << "3) Exit" << endl;
+		cin >> opt;
+   } 
+   while (opt != 1 && opt != 2 && opt != 3);
+   
+   return opt;
+}
+
+
+int menu2()
+{
+	int opt=0;
+	do{
+		cout << "1) Passenger" << endl;
+		cout << "2) Driver" << endl;
+		cin >> opt;
+	}
+	while(opt != 1 && opt != 2);
+
+	return opt;
+}
+
 int main()
 {
-   cout << "Hello World!\n";
+	int opt1 = menu1();
+
+	if (opt1 == 1) {
+		// ask details for signup
+	}
+	else if (opt1 == 2) {
+		// ask details for login
+	}
+	else
+		return 0;
 }
