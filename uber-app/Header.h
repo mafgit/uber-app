486b77327563622cda4fb23b9b5270ccf48455e7
mafgit <mafishere@gmail.com>
2023-03-06 23:24:34 +0500

    Added some todos Edited a few things Changed/added a few properties in classes

--- uber-app/Header.h
+++ uber-app/Header.h
@@ -2,25 +2,35 @@
 #include <iostream>
 using namespace std;
 
+// todo: inheritance (User -> Driver, Passenger, and if there's time and energy, then Admin);
+// basic variables will become protected in User class
 class Passenger {
 	int id, age;
 	string firstName, lastName, phoneNum, password;
-};
-
-
-class Driver {
-	int id, age, sumOfRatings, userCountOfRating;
-	string firstName, lastName, phoneNum;
+	// todo: nic?
 };
 
 class Vehicle {
 	int model, noOfSeats;
 	string brand, plateNum, color;
-	// todo: add name/type of vehicle like uber mini, etc 
+	// todo: add name/type of vehicle like uber mini, etc
+	// types: bike, ride_ac, ride, ride_mini, auto
+};
+
+class Driver {
+	int id, age;
+	string firstName, lastName, phoneNum, password;
+	// todo: nic?
+	int sumOfRatings, userCountOfRating;
+	Vehicle vehicle;
 };
 
 class Booking {
-	string type;
-	char pickup, dropoff;
-
+	char pickupLocation, dropoffLocation;
+	int fare; // in rupees, so I guess I do not need float
+	Passenger passenger;
+	Driver driver;
+	// todo: bookedAt (time of booking)
+	string status; // coming, ready, going, completed, cancelled
+	// todo: maybe I shouldn't add this many values for status
 };
--- uber-app/uber-app.cpp
+++ uber-app/uber-app.cpp
@@ -21,11 +21,14 @@ int menu1()
 int menu2()
 {
 	int opt = 0;
+
 	do {
-		cout << endl << "1) Passenger" << endl;
-		cout << "2) Driver" << endl;
+		cout << endl << "1) As passenger" << endl;
+		cout << "2) As driver" << endl;
+		cout << "3) Go Back" << endl;
 		cin >> opt;
-	} while (opt != 1 && opt != 2);
+	} while (opt != 1 && opt != 2 && opt != 3);
+	// todo: handle the "back" option
 
 	return opt;
 }
@@ -33,44 +36,55 @@ int menu2()
 int main()
 {
 	int opt1 = menu1();
+	if (opt1 == 3) return 0; // exit
+
 	int opt2 = menu2();
-	// todo: add "back" option in menu 2
+	// todo: handle the "back" option in menu 2
 
 	int id, age;
 	string firstName, lastName, phoneNum, password, confirmPassword;
 
-	if (opt1 == 1 || opt1 == 2) {
+	if (opt1 == 1) {
 		cout << endl << "Enter first name: ";
 		cin >> firstName;
 		cout << endl << "Enter last name: ";
 		cin >> lastName;
+		// todo: validations
+		// todo: think about reducing the code inside main and increasing the code inside classes (for later)
+		// todo: think about making validation functions separately in another file (for later maybe)
 
 		do {
-			cout <<  endl << "Enter age: ";
+			cout << endl << "Enter age: ";
 			cin >> age;
-			if(age < 18 && opt2 == 2)
+			if (age < 18 && opt2 == 2)
 				cout << endl << "Error: age of driver must be at least 18" << endl;
 		} while (age < 18 && opt2 == 2);
 
 		cout << endl << "Enter phone number: ";
 		cin >> phoneNum;
+		// todo: validation
 
 		do {
 			cout << endl << "Enter password: ";
 			cin >> password;
 			cout << endl << "Confirm password: ";
 			cin >> confirmPassword;
+			// todo: there is a problem when space is entered in string, so counter that problem in all string inputs
 
 			if (password.length() < 6)
 				cout << endl << "Error: password must be at least six characters long" << endl;
-			
+
 			if (password != confirmPassword)
 				cout << endl << "Error: passwords do not match" << endl;
 
 		} while (password.length() < 6 || password != confirmPassword);
-	}
 
-	// todo: check if driver is signing up, and in that case, ask for vehicle details
-	// todo: inheritance in header file
-	// todo: filing
-}		
+		// todo: input more details if the user is a driver, such as vehicle details, etc
+		// todo: check if phone no. exists already in file; in that case show that it exists already so try logging instead
+		// todo: otherwise append the details to end of the drivers or passengers file
+	}
+	else { // opt1 = 2 (login)
+		// todo: ask for login info
+		// it is already stored in opt2 whether the user wants to be logged in as driver or passenger
+	}
+}
