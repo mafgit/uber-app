eaaa41f565797a96fcc9d9ed970ff5b023d4c543
maf <61792345+mafgit@users.noreply.github.com>
2023-03-15 07:56:54 +0000

    Meeting 6: login getline changed to cin

--- uber-app/Header.h
+++ uber-app/Header.h
@@ -1,149 +1,179 @@
 #pragma once
 #include <iostream>
-#include<fstream>
+#include <fstream>
 
 using namespace std;
 
 // todo: inheritance (User -> Driver, Passenger, and if there's time and energy, then Admin);
 
-class User {
+class User
+{
 protected:
-	int id, day, month, year;
-	string firstName, lastName, phoneNum, nic, password;
-public:
-	User() {
-		id = -1;
-		day = 0;
-		month = 0;
-		year = 0;
-		firstName = "";
-		lastName = "";
-		phoneNum = "";
-		nic = "";
-		password = "";
-	}
-
-	User(int id, int day, int month, int year, string firstName, string lastName, string phoneNum, string password) : id(id), day(day), month(month), year(year), firstName(firstName), lastName(lastName), phoneNum(phoneNum), password(password) {}
-	
-	string getPhoneNum()
-	{
-		return phoneNum;
-	}
-};
-
-class Passenger : public User {
-public:
-	Passenger() {}
-
-	Passenger(int id, int day, int month, int year, string firstName, string lastName, string phoneNum, string password)
-		: User(id, day,  month,  year, firstName, lastName, phoneNum, password) {}
-
-	void appendToFile() {
-		ofstream passengers_cout("passengers.txt", ios::app);
-		passengers_cout.write((char*)this, sizeof(*this));
-		passengers_cout.close();
-	}
-
-};
-
-class Vehicle {
-	int model, noOfSeats;
-	string name, plateNum, color, type;
-	// types: bike, ride_ac, ride, ride_mini, auto
-	public:
-		Vehicle() {
-			// variable initialization
-		}
-		Vehicle(int model, int noOfSeats, string name, string plateNum, string color, string type){
-			this->model=model;
-			this->noOfSeats=noOfSeats;
-			this->name=name;
-			this->plateNum=plateNum;
-			this->color=color;
-			this->type=type;
-		}
-};
-
-class Driver : public User {
-	int sumOfRatings, userCountOfRating;
-	Vehicle vehicle;
-	string nic;
-	
+    int id, day, month, year;
+    string firstName, lastName, phoneNum, nic, password;
 
 public:
-	Driver() {
-		sumOfRatings=0;
-		userCountOfRating=0;
-		nic = "";
-		// todo: vehicle initialization
-	}
+    User()
+    {
+        id = -1;
+        day = 0;
+        month = 0;
+        year = 0;
+        firstName = "";
+        lastName = "";
+        phoneNum = "";
+        nic = "";
+        password = "";
+    }
 
-	Driver(int id, int day, int month, int year, string firstName, string lastName, string phoneNum, string password, Vehicle vehicle, string nic)
-		: User(id, day,  month,  year, firstName, lastName, phoneNum, password), sumOfRatings(0), userCountOfRating(0), vehicle(vehicle), nic(nic) {}
+    User(int id, int day, int month, int year, string firstName, string lastName, string phoneNum, string password) : id(id), day(day), month(month), year(year), firstName(firstName), lastName(lastName), phoneNum(phoneNum), password(password) {}
 
-	void appendToFile() {
-		ofstream drivers_cout("drivers.txt", ios::app);
-		drivers_cout.write((char*)this, sizeof(*this));
-		drivers_cout.close();
-	}
+    string getPhoneNum()
+    {
+    	cout << phoneNum;
+        return phoneNum;
+    }
+
+    string getPassword()
+    {
+    	cout << password;
+        return password;
+    }
 };
 
-class Booking {
-	char pickupLocation, dropoffLocation;
-	int fare; // todo: check if fare should be taken in float
-	Passenger passenger;
-	Driver driver;
-	// todo: bookedAt (time of booking)
-	// todo: completedAt
-	string status; // arriving, arrived, in_progress, completed, cancelled
+class Passenger : public User
+{
+public:
+    Passenger() {}
+
+    Passenger(int id, int day, int month, int year, string firstName, string lastName, string phoneNum, string password)
+        : User(id, day, month, year, firstName, lastName, phoneNum, password) {}
+
+    void appendToFile()
+    {
+        ofstream passengers_cout("passengers.txt", ios::app);
+        passengers_cout.write((char *)this, sizeof(*this));
+        passengers_cout.close();
+    }
 };
 
+class Vehicle
+{
+    int model, noOfSeats;
+    string name, plateNum, color, type;
+    // types: bike, ride_ac, ride, ride_mini, auto
+public:
+    Vehicle()
+    {
+        // variable initialization
+    }
+    
+    Vehicle(int model, int noOfSeats, string name, string plateNum, string color, string type)
+    {
+        this->model = model;
+        this->noOfSeats = noOfSeats;
+        this->name = name;
+        this->plateNum = plateNum;
+        this->color = color;
+        this->type = type;
+    }
+};
+
+class Driver : public User
+{
+    int sumOfRatings, userCountOfRating;
+    Vehicle vehicle;
+    string nic;
+
+public:
+    Driver()
+    {
+        sumOfRatings = 0;
+        userCountOfRating = 0;
+        nic = "";
+        // todo: vehicle initialization
+    }
+
+    Driver(int id, int day, int month, int year, string firstName, string lastName, string phoneNum, string password, Vehicle vehicle, string nic)
+        : User(id, day, month, year, firstName, lastName, phoneNum, password), sumOfRatings(0), userCountOfRating(0), vehicle(vehicle), nic(nic) {}
+
+    void appendToFile()
+    {
+        ofstream drivers_cout("drivers.txt", ios::app);
+        drivers_cout.write((char *)this, sizeof(*this));
+        drivers_cout.close();
+    }
+};
+
+class Booking
+{
+    char pickupLocation, dropoffLocation;
+    int fare; // todo: check if fare should be taken in float
+    Passenger passenger;
+    Driver driver;
+    // todo: bookedAt (time of booking)
+    // todo: completedAt
+    string status; // arriving, arrived, in_progress, completed, cancelled
+};
 
 int menu1()
 {
-	int opt;
+    int opt;
 
-	do {
-		cout << endl << "Enter a number for one of the following commands:" << endl;
-		cout << "1) Signup" << endl;
-		cout << "2) Login" << endl;
-		cout << "3) Exit" << endl;
-		cin >> opt;
-	} while (opt != 1 && opt != 2 && opt != 3);
+    do
+    {
+        cout << endl
+             << "Enter a number for one of the following commands:" << endl;
+        cout << "1) Signup" << endl;
+        cout << "2) Login" << endl;
+        cout << "3) Exit" << endl;
+        cin >> opt;
+    } while (opt != 1 && opt != 2 && opt != 3);
 
-	return opt;
+    return opt;
 }
 
 int menu2()
 {
-	int opt = 0;
+    int opt = 0;
 
-	do {
-		cout << endl << "1) As passenger" << endl;
-		cout << "2) As driver" << endl;
-		cout << "3) Go Back" << endl;
-		cin >> opt;
-	} while (opt != 1 && opt != 2 && opt != 3);
+    do
+    {
+        cout << endl
+             << "1) As passenger" << endl;
+        cout << "2) As driver" << endl;
+        cout << "3) Go Back" << endl;
+        cin >> opt;
+    } while (opt != 1 && opt != 2 && opt != 3);
 
-	return opt;
+    return opt;
 }
 
-string typesMenu() {
-	int opt = 0;
+string typesMenu()
+{
+    int opt = 0;
 
-	cout << endl << "Enter a number for the type of your vehicle: " << endl;
-	do {
-		cout << endl << "1) Bike" << endl;
-		cout << "2) Ride AC" << endl;
-		cout << "3) Ride" << endl;
-		cout << "4) Ride Mini" << endl;
-		cout << "5) Auto" << endl;
-		cin >> opt;
-	} while (opt < 1 || opt > 5);
+    cout << endl
+         << "Enter a number for the type of your vehicle: " << endl;
+    do
+    {
+        cout << endl
+             << "1) Bike" << endl;
+        cout << "2) Ride AC" << endl;
+        cout << "3) Ride" << endl;
+        cout << "4) Ride Mini" << endl;
+        cout << "5) Auto" << endl;
+        cin >> opt;
+    } while (opt < 1 || opt > 5);
 
-	if (opt == 1) return "Bike";
-	else if (opt == 2) return "Ride AC";
-	else if (opt == 3) return "Ride";
-	else if (opt == 4) return "Ride Mini";
-	else return "Auto";
+    if (opt == 1)
+        return "Bike";
+    else if (opt == 2)
+        return "Ride AC";
+    else if (opt == 3)
+        return "Ride";
+    else if (opt == 4)
+        return "Ride Mini";
+    else
+        return "Auto";
 }
--- uber-app/uber-app.cpp
+++ uber-app/uber-app.cpp
@@ -6,12 +6,15 @@ using namespace std;
 
 int main()
 {
-	cout << endl << "Welcome to Uber Application!" << endl;
+	cout << endl
+		 << "Welcome to Uber Application!" << endl;
 	int opt1, opt2;
 
-	do {
+	do
+	{
 		opt1 = menu1();
-		if (opt1 == 3) return 0; // exit
+		if (opt1 == 3)
+			return 0; // exit
 		opt2 = menu2();
 	} while (opt2 == 3);
 
@@ -20,12 +23,16 @@ int main()
 	string nic;
 
 	bool is_err;
-	if (opt1 == 1) {
-		do {
-			cout << endl << "Enter first name: ";
+	if (opt1 == 1)
+	{
+		do
+		{
+			cout << endl
+				 << "Enter first name: ";
 			cin >> firstName;
 
-			cout << endl << "Enter last name: ";
+			cout << endl
+				 << "Enter last name: ";
 			cin >> lastName;
 
 			// todo: validation of names regarding spaces
@@ -36,74 +43,95 @@ int main()
 		// todo: think about reducing the code inside main and increasing the code inside classes (for later)
 		// todo: think about making validation functions separately in another file (for later maybe)
 
-		do {
-			cout << endl << "Enter date of your date of birth: ";
+		do
+		{
+			cout << endl
+				 << "Enter date of your date of birth: ";
 			cin >> day;
-			cout << endl << "Enter month of your date of birth: ";
+			cout << endl
+				 << "Enter month of your date of birth: ";
 			cin >> month;
-			cout << endl << "Enter year of your date of birth: ";
+			cout << endl
+				 << "Enter year of your date of birth: ";
 			cin >> year;
 
 			is_err = day < 1 || day > 31 || month < 1 || month > 12 || year > 2023 || year < 1920;
 
 			if (is_err)
-				cerr << endl << "Error: invalid date of birth" << endl;
+				cerr << endl
+					 << "Error: invalid date of birth" << endl;
 
 			// todo: make 2023 and 1920 into a variable
 			// todo: check if driver is of at least 18 years old
 		} while (is_err);
 
+		bool flag1;
+		bool flag2;
+		bool phoneNumExists;
 
-		if (opt2 == 2) {
-			bool flag1;
-			bool flag2;
-			bool phoneNumExists;
+		do
+		{
+			flag1 = false;
+			flag2 = false;
+			phoneNumExists = false;
 
-			do {
-				flag1 = false;
-				flag2 = false;
-				phoneNumExists = false;
+			cout << endl
+				 << "Enter phone number (start with 0): ";
+			cin >> phoneNum;
 
-				cout << endl << "Enter phone number (start with 0): ";
-				cin >> phoneNum;
-
-				if (phoneNum[0] != '0' || phoneNum[1] != '3')
-					flag1 = true;
-				else {
-					for (int i = 0; i < phoneNum.length(); i++) {
-						if (!isdigit(phoneNum[i])) {
-							flag2 = true;
-							break;
-						}
+			if (phoneNum[0] != '0' || phoneNum[1] != '3')
+				flag1 = true;
+			else
+			{
+				for (int i = 0; i < phoneNum.length(); i++)
+				{
+					if (!isdigit(phoneNum[i]))
+					{
+						flag2 = true;
+						break;
 					}
 				}
+			}
 
-				is_err = phoneNum.length() != 11 || flag1 == true || flag2 == true;
-				if (is_err)
-					cerr << endl << "Error: invalid phone number" << endl;
-				else {
-					if (opt2 == 1) {
-						ifstream passengers_cin("passengers.txt");
+			is_err = phoneNum.length() != 11 || flag1 == true || flag2 == true;
+			if (is_err)
+				cerr << endl
+					 << "Error: invalid phone number" << endl;
+			else
+			{
+				if (opt2 == 1)
+				{
+					ifstream passengers_cin("passengers.txt");
+					if (passengers_cin)
+					{
 						Passenger passenger;
-
-						while (passengers_cin.eof() == 0) {
-							passengers_cin.read((char*)&passenger, sizeof(passenger));
-							if (passenger.getPhoneNum() == phoneNum) {
-								cerr << endl << "Error: passenger already exists with this phone number" << endl;
+						while (passengers_cin.eof() == 0)
+						{
+							passengers_cin.read((char *)&passenger, sizeof(passenger));
+							if (passenger.getPhoneNum() == phoneNum)
+							{
+								cerr << endl
+									 << "Error: passenger already exists with this phone number" << endl;
 								phoneNumExists = true;
 								break;
 							}
 						}
 						passengers_cin.close();
 					}
-					else if (opt2 == 2) {
-						ifstream drivers_cin("drivers.txt");
+				}
+				else if (opt2 == 2)
+				{
+					ifstream drivers_cin("drivers.txt");
+					if (drivers_cin)
+					{
 						Driver driver;
-
-						while (drivers_cin.eof() == 0) {
-							drivers_cin.read((char*)&driver, sizeof(driver));
-							if (driver.getPhoneNum() == phoneNum) {
-								cerr << endl << "Error: driver already exists with this phone number" << endl;
+						while (drivers_cin.eof() == 0)
+						{
+							drivers_cin.read((char *)&driver, sizeof(driver));
+							if (driver.getPhoneNum() == phoneNum)
+							{
+								cerr << endl
+									 << "Error: driver already exists with this phone number" << endl;
 								phoneNumExists = true;
 								break;
 							}
@@ -111,19 +139,27 @@ int main()
 						drivers_cin.close();
 					}
 				}
-			} while (is_err || phoneNumExists);
+			}
+		} while (is_err || phoneNumExists);
 
-			do {
+		if (opt2 == 2)
+		{
+			do
+			{
 				flag1 = false;
 				flag2 = false;
-				cout << endl << "Enter NIC number (without dashes): ";
+				cout << endl
+					 << "Enter NIC number (without dashes): ";
 				cin >> nic;
 
 				if (nic[0] != '4')
 					flag1 = true;
-				else {
-					for (int i = 0; i < nic.length(); i++) {
-						if (!isdigit(nic[i])) {
+				else
+				{
+					for (int i = 0; i < nic.length(); i++)
+					{
+						if (!isdigit(nic[i]))
+						{
 							flag2 = true;
 							break;
 						}
@@ -133,28 +169,33 @@ int main()
 				is_err = nic.length() != 13 || flag1 || flag2;
 
 				if (is_err)
-					cerr << endl << "Error: invalid NIC" << endl;
+					cerr << endl
+						 << "Error: invalid NIC" << endl;
 			} while (is_err);
 		}
 
-		cin.ignore();
+		do
+		{
+			cout << endl
+				 << "Enter password: ";
+			cin >> password;
 
-		do {
-			cout << endl << "Enter password: ";
-			getline(cin, password);
-
-			cout << endl << "Confirm password: ";
-			getline(cin, confirmPassword);
+			cout << endl
+				 << "Confirm password: ";
+			cin >> confirmPassword;
 
 			if (password.length() < 6)
-				cerr << endl << "Error: password must be at least six characters long" << endl;
+				cerr << endl
+					 << "Error: password must be at least six characters long" << endl;
 
 			if (password != confirmPassword)
-				cerr << endl << "Error: passwords do not match" << endl;
+				cerr << endl
+					 << "Error: passwords do not match" << endl;
 
 		} while (password.length() < 6 || password != confirmPassword);
 
-		if (opt2 == 2) { // driver
+		if (opt2 == 2)
+		{ // driver
 			int model, noOfSeats;
 			string plateNum, color, type, name;
 
@@ -181,14 +222,80 @@ int main()
 			Driver driver(0, day, month, year, firstName, lastName, phoneNum, password, vehicle, nic);
 			driver.appendToFile();
 		}
-		else { // passenger
+		else
+		{ // passenger
 			// todo: keep check of id and change 0 to id in next line
 			Passenger passenger(0, day, month, year, firstName, lastName, phoneNum, password);
 			passenger.appendToFile();
 		}
 	}
-	else { // opt1 = 2 (login)
+	else
+	{ // opt1 = 2 (login)
 		// todo: ask for login info
 		// it is already stored in opt2 whether the user wants to be logged in as driver or passenger
+		cout << endl
+			 << "Enter phone number: " << endl;
+		cin >> phoneNum;
+
+		cout << endl << "Enter password: " << endl;
+		cin >> password;
+
+		if (opt2 == 1)
+		{
+			bool found = 0;
+			ifstream passengers_cin("passengers.txt");
+			Passenger passenger;
+			while (!passengers_cin.eof() && !found)
+			{
+				passengers_cin.read((char *)&passenger, sizeof(passenger));
+				cout << passenger.getPassword() << passenger.getPhoneNum();
+				if (password == passenger.getPassword() && phoneNum == passenger.getPhoneNum())
+				{
+					found = 1;
+				}
+			}
+
+			passengers_cin.close();
+
+			if (!found)
+			{
+				cerr << endl
+					 << "Error: invalid credentials" << endl;
+			}
+			else
+			{
+				// logged in as passenger
+				cout << "Logged in as passenger";
+				// todo: correct error
+			}
+		}
+		else if (opt2 == 2)
+		{
+			bool found = 0;
+			ifstream drivers_cin("drivers.txt");
+			Driver driver;
+			while (!drivers_cin.eof() && !found)
+			{
+				drivers_cin.read((char *)&driver, sizeof(driver));
+				if (password == driver.getPassword() && phoneNum == driver.getPhoneNum())
+				{
+					found = 1;
+				}
+			}
+
+			drivers_cin.close();
+
+			if (!found)
+			{
+				cerr << endl
+					 << "Error: invalid credentials" << endl;
+			}
+			else
+			{
+				// logged in as driver
+				cout << "Logged in as driver";
+				// todo: correct error
+			}
+		}
 	}
 }
